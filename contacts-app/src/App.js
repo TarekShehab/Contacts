bb767d2b2052f5764f7b0bbab79f5ff9a31ba94e
Tarek <tarek.shehabeldin.ibrahim@gmail.com>
2022-08-03 00:47:05 +0200

    Added contacts to app state

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "soka el 3abeta",
-    "name": "Soka el 3abeta",
-    "handle": "@Soka_!!!",
-    "avatarURL": "https://placekitten.com/1000"
-    // "avatarURL": "https://placebeard.it/250*147"
-    // "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "Ali Alokka",
-    "name": "Ali Alokka",
-    "handle": "@A_Alokka",
-    "avatarURL": "https://placebeard.it/250*147"
-    // "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "Ashraf Kokha",
-    "name": "Ashraf Kokha",
-    "handle": "@Ash_Kokha",
-    "avatarURL": "https://placebeard.it/200*118"
-    // "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
-
 class App extends Component {
+  state={
+    contacts: [
+      {
+        "id": "soka el 3abeta",
+        "name": "Soka el 3abeta",
+        "handle": "@Soka_!!!",
+        "avatarURL": "https://placekitten.com/1000"
+        // "avatarURL": "https://placebeard.it/250*147"
+        // "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "Ali Alokka",
+        "name": "Ali Alokka",
+        "handle": "@A_Alokka",
+        "avatarURL": "https://placebeard.it/250*147"
+        // "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "Ashraf Kokha",
+        "name": "Ashraf Kokha",
+        "handle": "@Ash_Kokha",
+        "avatarURL": "https://placebeard.it/200*118"
+        // "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+     ]
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
