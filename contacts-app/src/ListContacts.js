e7312a81f51942ea49bccb0c36654201807f79c3
Tarek <tarek.shehabeldin.ibrahim@gmail.com>
2022-08-03 01:17:46 +0200

    removeContact function added and passed to the remove button

--- contacts-app/src/ListContacts.js
+++ contacts-app/src/ListContacts.js
@@ -17,7 +17,7 @@ function ListContacts(props) {
                             <p>{contact.name}</p>
                             <p>{contact.handle}</p>
                         </div>
-                        <button className='contact-remove'>Remove</button>
+                        <button className='contact-remove' onClick={() => props.onDeleteContact(contact)}>Remove</button>
                     </li> 
                 ))
             }
