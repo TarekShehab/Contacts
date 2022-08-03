4e1cd349e186f91fd8385c895d3c003b6834c4b2
Tarek <tarek.shehabeldin.ibrahim@gmail.com>
2022-08-03 04:40:34 +0200

    Search bar added

--- contacts-app/src/ListContacts.js
+++ contacts-app/src/ListContacts.js
@@ -1,34 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import propTypes from 'prop-types'
 
-function ListContacts(props) {
-    return (
-        <ol className='contact-list'>
-            { 
-                props.contacts.map(contact => (
-                    <li key={contact.id} className='contact-list-item'>
-                        <div 
-                            className='contact-avatar'
-                            style={{
-                                // backgroundImage: `url('./icons/arrow-back.svg')`
-                                backgroundImage: `url(${contact.avatarURL})`
-                            }}
-                        ></div>
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.handle}</p>
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: propTypes.array.isRequired,
+        onDeleteContact: propTypes.func.isRequired, 
+    }
+   
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState(() => ({
+            query: query.trim()
+        }))
+    }
+
+    clearQuery = () => {
+        this.updateQuery(' ')
+    }
+
+    render() {
+
+        
+        const { query } = this.state
+        const { contacts, onDeleteContact } = this.props
+
+        const ShowingContacts = query === '' ? contacts : contacts.filter(c => (c.name.toLowerCase().includes(query.toLocaleLowerCase()))) 
+
+        return (
+            <div className='list-contacts'>
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts'
+                        type='text'
+                        placeholder='Search Contacts'
+                        value={query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+
+                {
+                    (ShowingContacts.length !== contacts.length) && (
+                        <div className='showing-contacts'>
+                            <span> Now Showing {ShowingContacts.length} of {contacts.length}</span>
+                            <button onClick={this.clearQuery}>Show all again</button>
                         </div>
-                        <button className='contact-remove' onClick={() => props.onDeleteContact(contact)}>Remove</button>
-                    </li> 
-                ))
-            }
-        </ol>
-    )
+                    )
+                }
+
+                <ol className='contact-list'>
+                    { 
+                        ShowingContacts.map(contact => (
+                            <li key={contact.id} className='contact-list-item'>
+                                <div 
+                                    className='contact-avatar'
+                                    style={{
+                                        // backgroundImage: `url('./icons/arrow-back.svg')`
+                                        backgroundImage: `url(${contact.avatarURL})`
+                                    }}
+                                ></div>
+                                <div className='contact-details'>
+                                    <p>{contact.name}</p>
+                                    <p>{contact.handle}</p>
+                                </div>
+                                <button className='contact-remove' onClick={() => onDeleteContact(contact)}>Remove</button>
+                            </li> 
+                        ))
+                    }
+                </ol>
+            </div>
+        )
+
+    }
+
 }
 
-ListContacts.propTypes = {
-    contacts: propTypes.array.isRequired,
-    onDeleteContact: propTypes.func.isRequired,
-}
 
 export default ListContacts;
